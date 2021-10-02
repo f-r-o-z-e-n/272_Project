cb434e0802d6c33882e075a1b0686860fed7e3e8
f-r-o-z-e-n <afrozshaukat.inamdar@sjsu.edu>
2021-10-01 21:11:35 -0700

    adding index file

--- index.php
+++ index.php
@@ -1,1 +1,62 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/index.css" />
+
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+      crossorigin="anonymous"
+    />
+    <title>Meetup</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row align-items-start" style="background-color: teal">
+        <div class="col-2">
+          <img src="Images/logo.jpg" class="logo" />
+        </div>
+        <div class="col-10">
+          <div class="row header-text">
+            <h2>Charlotte Data Sciece MeetUp</h2>
+          </div>
+          <div class="row">
+            <nav class="nav justify-content-end">
+              <a class="nav-link active nav-link" href="./form.html"
+                >New Event</a
+              >
+              <a class="nav-link nav-link" href="#">Login</a>
+              <a class="nav-link nav-link" href="#">Signup</a>
+            </nav>
+          </div>
+          <div class="row">
+            <nav class="nav justify-content-start">
+              <a class="nav-link active nav-link" href="./index.html">Home</a>
+              <a class="nav-link active nav-link" href="./events.html"
+                >Upcomming Events</a
+              >
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div class="paragrph">
+        <pre><b>
+          Welcome to Charlotte meet-up community. We connect like-minded people who are
+          interested in Data Science and Artificial Intelligence. We frequently
+          organize meetups, workshops related to this amazing field. Come and
+          join us</b>
+        </pre>
+      </div>
+      <div class="row align-items-center">
+        <p></p>
+      </div>
+      <div class="footer">
+        <p>@copyright: gmore@uncc.edu</p>
+      </div>
+    </div>
+  </body>
+</html>
